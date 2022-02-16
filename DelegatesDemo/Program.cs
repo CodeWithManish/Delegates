e9670f695014f6485b1e1e5f301cd42ceb07b112
Manish <manishkumar0156@gmail.com>
2022-02-16 16:47:43 +0530

    [Manish] Single Delegates Operation

---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Delegates Operation!");
+            Console.WriteLine("Enter 1. to print sinle Delegates\n");
+            bool flag = true;
+            while (flag)
+            {
+                int check = Convert.ToInt32(Console.ReadLine());
+
+                switch (check)
+                {
+                    case 1:
+                        SingleDelegates single=new SingleDelegates();   
+                        single.Delegates();
+                        break;
+
+
+
+                }
+            }
+        }
+    }
+}
---
+++ DelegatesDemo/SingleDelegates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    public class SingleDelegates
+    {
+        delegate void DelOperation(int x, int y);
+
+        public void Delegates()
+        {
+            DelOperation[] obj ={
+                new DelOperation(Operation.Add),
+                new DelOperation(Operation.Multiple)
+            };
+
+            for (int i = 0; i < obj.Length; i++)
+            {
+                obj[i](2, 5);
+                obj[i](8, 5);
+                obj[i](4, 6);
+            }
+        }
+    }
+}
